e5cbf825b2f0e86a40536d5c7c2e744b4d82c6df
Monica Rathbun @SQLEspresso <mrathbun.dba@gmail.com>
2019-10-09 09:42:11 -0400

    Add files via upload

--- 03b-Stats IO.sql
+++ 03b-Stats IO.sql
@@ -10,8 +10,8 @@
 --------------------------------------------------------------------------------------------------
 --CLEAN UP Just in case
 --------------------------------------------------------------------------------------------------
-IF EXISTS (SELECT name from sys.indexes where name = N'IDX_UnitPrice_UnitPriceDiscount')
-DROP INDEX IDX_UnitPrice_UnitPriceDiscount ON [Sales].[SalesOrderDetail]
+IF EXISTS (SELECT name from sys.indexes where name = N'IDX_ProductID_UnitPrice_UnitPriceDiscount')
+DROP INDEX IDX_ProductID_UnitPrice_UnitPriceDiscount ON [Sales].[SalesOrderDetail]
 
 
 --------------------------------------------------------------------------------------------------
@@ -31,10 +31,11 @@ INNER JOIN [Production].[ProductDescription] pd
 INNER JOIN [Sales].[SalesOrderDetail] s
 	ON p.[ProductID]=s.ProductID
 WHERE SellEndDate is not NULL
-AND UnitPrice>100.00 
+AND UnitPrice>10.00 
 AND UnitPriceDiscount<>0
 ORDER BY [Name],[UnitPrice]
 GO
+
 
 --------------------------------------------------------------------------------------------------
 -- Add Index
@@ -42,9 +43,8 @@ GO
 
 USE [AdventureWorks2016CTP3]
 GO
-CREATE NONCLUSTERED INDEX IDX_UnitPrice_UnitPriceDiscount
-ON [Sales].[SalesOrderDetail] ([UnitPrice],[UnitPriceDiscount])
-INCLUDE ([ProductID])
+CREATE NONCLUSTERED INDEX IDX_ProductID_UnitPrice_UnitPriceDiscount
+ON [Sales].[SalesOrderDetail] ([ProductID],[UnitPrice],[UnitPriceDiscount])
 GO
 
 
@@ -70,3 +70,32 @@ AND UnitPrice>10.00
 AND UnitPriceDiscount<>0
 ORDER BY [Name],[UnitPrice]
 GO
+--------------------------------------------------------------------------------------------------
+--BEFORE INDEX
+--------------------------------------------------------------------------------------------------
+
+/* SQL Server Execution Times:
+   CPU time = 31 ms,  elapsed time = 544 ms.
+SQL Server parse and compile time: 
+   CPU time = 0 ms, elapsed time = 0 ms
+
+Table 'Worktable'. Scan count 0, logical reads 0, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+Table 'Workfile'. Scan count 0, logical reads 0, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+Table 'SalesOrderDetail'. Scan count 1, logical reads 455, physical reads 0, read-ahead reads 4, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+Table 'ProductDescription'. Scan count 0, logical reads 211, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+Table 'Product'. Scan count 1, logical reads 15, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+*/
+
+--------------------------------------------------------------------------------------------------
+--AFTER INDEX
+--------------------------------------------------------------------------------------------------
+/*  SQL Server Execution Times:
+   CPU time = 0 ms,  elapsed time = 176 ms.
+SQL Server parse and compile time: 
+   CPU time = 0 ms, elapsed time = 0 ms.
+
+Table 'SalesOrderDetail'. Scan count 10, logical reads 38, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+Table 'Worktable'. Scan count 0, logical reads 0, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+Table 'ProductDescription'. Scan count 0, logical reads 211, physical reads 0, read-ahead reads 8, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+Table 'Product'. Scan count 1, logical reads 15, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+*/
